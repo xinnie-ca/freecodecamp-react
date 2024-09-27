f620364358e6831e923f1928bf513e16238c9995
XinNie <umnie2@myumanitoba.ca>
2024-09-27 15:10:39 -0500

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Main() {
+  return <h1>main goes here</h1>;
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../images/logo.svg";
+
+export default function NavBar() {
+  return (
+    <nav className="nav">
+      <img className="nav--icon" src={logo} alt="No!! sth wrong"></img>
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React course - Project 1</h4>
+    </nav>
+  );
+}
